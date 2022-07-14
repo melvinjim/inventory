f3c2d2fa466e2b66d7ef588a83492ca20cddf56d
melvinjim <melvinjimenezang@hotmail.com>
2022-07-14 16:58:34 -0500

    changes in the html respect add line

--- actions/list_line.go
+++ actions/list_line.go
@@ -36,3 +36,31 @@ func InfoLine(c buffalo.Context) error {
 
 	return c.Render(http.StatusOK, r.HTML("list_lines/info_line.plush.html"))
 }
+
+func AddLine(c buffalo.Context) error {
+	line := &models.Lines{}
+	c.Set("line", line)
+
+	return c.Render(http.StatusOK, r.HTML("list_lines/add_line.plush.html"))
+}
+
+func CreateLine(c buffalo.Context) error {
+	tx := c.Value("tx").(*pop.Connection)
+
+	line := models.Lines{}
+	if err := c.Bind(&line); err != nil {
+		return err
+	}
+
+	verrs, err := tx.ValidateAndCreate(&line)
+	if err != nil {
+		return err
+	}
+
+	if verrs.HasAny() {
+		c.Set("errors", verrs.Get("number"))
+		return c.Render(http.StatusOK, r.HTML("list_lines/add_line.plush.html"))
+	}
+
+	return c.Redirect(http.StatusSeeOther, "/")
+}
--- models/table_line.go
+++ models/table_line.go
@@ -3,18 +3,34 @@ package models
 import (
 	"time"
 
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
 	"github.com/gofrs/uuid"
 )
 
 type Lines struct {
-	ID                 uuid.UUID `json:"id" db:"id"`
-	PhoneLine          string    `json:"phone_line" db:"phone_line"`
-	Carrier            string    `json:"carrier" db:"carrier"`
-	AssociatedTo       string    `json:"associated_to" db:"associated_to"`
-	AssociatedToDevice string    `json:"associated_to_device" db:"associated_to_device"`
-	EndContractDate    string    `json:"end_contract_date" db:"end_contract_date"`
-	UpgradeEligibility string    `json:"upgrade_eligibility" db:"upgrade_eligibility"`
-	IMEI               string    `json:"imei" db:"imei"`
+	ID                 uuid.UUID `form:"id" db:"id"`
+	PhoneLine          string    `form:"phone_line" db:"phone_line"`
+	Carrier            string    `form:"carrier" db:"carrier"`
+	AssociatedTo       string    `form:"associated_to" db:"associated_to"`
+	AssociatedToDevice string    `form:"associated_to_device" db:"associated_to_device"`
+	EndContractDate    string    `form:"end_contract_date" db:"end_contract_date"`
+	UpgradeEligibility string    `form:"upgrade_eligibility" db:"upgrade_eligibility"`
+	IMEI               string    `form:"imei" db:"imei"`
 	CreatedAt          time.Time `form:"created_at" db:"created_at"`
 	UpdatedAt          time.Time `form:"updated_at" db:"updated_at"`
+}
+
+func (l *Lines) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(&validators.StringIsPresent{
+		Field:   l.PhoneLine,
+		Name:    "number",
+		Message: "This field is required",
+	}), nil
+}
+
+func (l *Lines) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+
+	return validate.NewErrors(), nil
 }
